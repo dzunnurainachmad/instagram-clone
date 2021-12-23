f0cfc29c86f67ec59c7189ef9bf2a996cf4f12d3
dzunnurainaaz <dzunnurainaaz@gmail.com>
2021-12-23 16:32:53 +0700

    add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGN0-xKcYlJC6wevZbSapAuqpAMabO5WA",
+  authDomain: "instagram-clone-e3781.firebaseapp.com",
+  projectId: "instagram-clone-e3781",
+  storageBucket: "instagram-clone-e3781.appspot.com",
+  messagingSenderId: "543336044937",
+  appId: "1:543336044937:web:38aecffe43c6767e086958"
+};
+
+// Initialize Firebase
+export const firebase = initializeApp(firebaseConfig);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import {  ref } from 'vue';
+
+const msg = ref('Hello')
+const change = (val: string) => msg.value = val
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div @click="change('asdf')">
+      {{msg}}
+    </div>
   </main>
 </template>
