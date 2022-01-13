f39b598692565d5ae5892466569de86b33f6c0ae
dzunnurainaaz <dzunnurainaaz@gmail.com>
2022-01-13 17:17:05 +0700

    fix navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,19 +10,21 @@ const userStore = useUserStore()
 </script>
 <template>
   <div class="h-14 w-full border-b drop-shadow-sm flex justify-center items-center">
-    <div class="w-9/12">
+    <div class="sm:w-9/12 w-full">
       <div class="flex justify-between">
-        <div class="flex items-center">
+        <div class="flex items-center px-2">
           <img src="../assets/Instagram_logo.svg" alt="" class="w-24">
         </div>
         <div class="flex justify-center" v-if="mq.current !== 'sm'">
-          <div class="flex bg-gray-200 rounded-md w-28 px-2 py-1">
-            <Icon icon="akar-icons:search" width="24"  />
+          <div class="flex bg-gray-200 rounded-md w-28 px-2">
+            <div class="flex items-center">
+              <Icon icon="akar-icons:search" width="16"   />
+            </div>
             <input type="text" placeholder="search" class="pl-2 w-20 rounded-sm bg-gray-200">
 
           </div>
         </div>
-        <div class="flex">
+        <div class="flex px-2">
           <div class="pr-2 flex items-center">
             <Icon icon="mdi:home-variant-outline" width="26" />
           </div>
@@ -35,7 +37,7 @@ const userStore = useUserStore()
           <div class="px-2 flex items-center">
             <Icon icon="mdi:heart-outline" width="26" />
           </div>
-          <div class="pl-2">
+          <div class="pl-2 flex items-center">
             <img :src="userStore.user?.photoURL" alt="" class="rounded-full w-8">
           </div>
         </div>
