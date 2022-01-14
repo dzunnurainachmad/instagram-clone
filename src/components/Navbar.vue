dfbac045a2c5c0745968a7371e85c5e921fe2ddc
dzunnurainaaz <dzunnurainaaz@gmail.com>
2022-01-14 15:17:04 +0700

    add v-click outside for pop up

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,6 +5,13 @@ import { ref } from 'vue';
 import { useMq } from 'vue3-mq'
 const mq = useMq()
 const userStore = useUserStore()
+const profilePop = ref(false)
+const openProfilePop = () => {
+  profilePop.value = !profilePop.value
+}
+const closeProfilePop = () => {
+  profilePop.value = false
+}
 
 
 </script>
@@ -16,29 +23,45 @@ const userStore = useUserStore()
           <img src="../assets/Instagram_logo.svg" alt="" class="w-24">
         </div>
         <div class="flex justify-center" v-if="mq.current !== 'sm'">
-          <div class="flex bg-gray-200 rounded-md w-28 px-2">
+          <div class="flex bg-gray-200 rounded-md w-32 px-2">
             <div class="flex items-center">
               <Icon icon="akar-icons:search" width="16"   />
             </div>
-            <input type="text" placeholder="search" class="pl-2 w-20 rounded-sm bg-gray-200">
+            <input type="text" placeholder="Search" class="pl-2 w-24 outline-none rounded-sm bg-gray-200">
 
           </div>
         </div>
         <div class="flex px-2">
           <div class="pr-2 flex items-center">
-            <Icon icon="mdi:home-variant-outline" width="26" />
+            <Icon icon="mdi:home-variant-outline" width="24" />
           </div>
           <div class="px-2 flex items-center">
-            <Icon icon="mdi:message-processing-outline" width="26" />
+            <Icon icon="mdi:message-processing-outline" width="24" />
           </div>
           <div class="px-2 flex items-center">
-            <Icon icon="mdi:compass-outline" width="26" />
+            <Icon icon="mdi:compass-outline" width="24" />
           </div>
           <div class="px-2 flex items-center">
-            <Icon icon="mdi:heart-outline" width="26" />
+            <Icon icon="mdi:heart-outline" width="24" />
           </div>
-          <div class="pl-2 flex items-center">
-            <img :src="userStore.user?.photoURL" alt="" class="rounded-full w-8">
+          <div class="pl-2 flex flex-col justify-center relative">
+            <div class="">
+              <img :src="userStore.user?.photoURL" alt="" class="rounded-full w-7 cursor-pointer" :class="`${profilePop? 'outline outline-gray-500': ''}`" @click.self="openProfilePop">
+            </div>
+            <div v-if="profilePop" class="absolute w-40 top-10 -right-5 border bg-white shadow-sm rounded cursor-pointer" v-click-outside="closeProfilePop">
+              <div class="py-2 px-2 flex hover:bg-gray-100">
+                <Icon icon="iconoir:profile-circled" width="20" />
+                <div class="pl-2 text-sm">Profile</div>
+              </div>
+              <div class="py-2 px-2 flex hover:bg-gray-100">
+                <Icon icon="mdi:bookmark-outline" width="20" />
+                <div class="pl-2 text-sm">Saved</div>
+              </div>
+              <div class="py-2 px-2 flex hover:bg-gray-100">
+                <Icon icon="mdi:cog" width="20" />
+                <div class="pl-2 text-sm">Settings</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { Vue3Mq } from 'vue3-mq'
 import './firebase.ts'
 import App from './App.vue'
 import router from './router'
+import vClickOutside from "click-outside-vue3"
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(vClickOutside)
 app.use(Vue3Mq, {
   breakpoints: {
     sm: 0,
